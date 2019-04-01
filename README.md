47cfb32769a70534cbc277724c1b2a2a148ccda4
Panda <pandav0x@hotmail.fr>
2019-04-02 00:20:40 +0200

    Retriveing urls, but insert multiple time

--- README.md
+++ README.md
@@ -12,7 +12,8 @@
         {
             "animeList":       "http://animewebsite.com/animes-list",
             "animeBasePage":   "http://animewebsite.com/tvshows/",
-            "animeEpPage":     "http://animewebsite.com/episodes/"
+            "animeEpPage":     "http://animewebsite.com/episodes/",
+            "repoUrl":         "http://baseanimerepowebsite.com/"
         }
     - Replace `animewebsite.com` by your source.
 - You can type `node index.js` (provided that your mysql server is runing on the `:3306` port)
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+'use strict';
+
 var app = require('express')();
 const fs = require('fs');
 var http = require('http').Server(app);
 var mysql = require('mysql');
 var Crawler = require("crawler");
+var Base64 = require('base-64');
 
 var animeList = [];
+var animeEpisodeList = [];
 
 var rawdata = fs.readFileSync('urls.json');  
 var urls = JSON.parse(rawdata); 
@@ -15,7 +19,8 @@ var urls = JSON.parse(rawdata);
 var dbconn = mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
-    password: ""
+    password: "",
+    database: "animepanda"
 });
 
 dbconn.connect(function(err) {
@@ -34,8 +39,12 @@ var crawler = new Crawler();
  */
 console.log("Finding anime list");
 crawler.queue([{
-    uri: urls["animeList"], 
+    uri: urls["animeList"],
+    retries: 0,
+    maxConnections : 1,
+    skipEventRequest: false,
     callback: function (error, res, done) {
+        console.log("start retrieving anime list");
         if(error)
             console.log(error);
         else
@@ -43,26 +52,80 @@ crawler.queue([{
             var $ = res.$;
             //TODO: instert raw name in db
             animeList = $(".anm_det_pop.pop_info")
-                .text()
-                .toLowerCase()
-                .replace(/!|:|;|\-|–|'|&|>|<|…|’|,|\?/g, '')
-                .replace(/^[\ ]*/gm, '')
-                .replace(/[\ ]*$/gm, '')
-                .replace(/\ /g, '-')
-                .split("\n")
-                .filter(function(entry) { 
-                    return entry.trim() != ''; 
+                            .text()
+                            .toLowerCase()
+                            .replace(/!|:|;|\-|–|'|&|>|<|…|’|,|\?|"|`|%22/g, '')
+                            .replace(/^[\ ]*/gm, '')
+                            .replace(/[\ ]*$/gm, '')
+                            .replace(/\ /g, '-')
+                            .replace(/\r/g, '')
+                            .split("\n")
+                            .filter(function(entry) { 
+                                return entry.trim() != ''; 
+                            });
+            animeList.forEach(function(element){
+                if(element.includes("☆","♥","∞"))
+                    return;
+                crawler.queue({
+                    uri: (urls["animeBasePage"] + element),
+                    retries: 0,
+                    maxConnections : 5,
+                    rateLimit: 1000,
+                    retryTimeout: 5000,
+                    callback: function (error, res, done) {
+                        if(error)
+                            return;
+                        else
+                        {
+                            var $ = res.$;
+                            if($("article.item>div.poster>div.season_m>a") == null)
+                                return;
+                            $("article.item>div.poster>div.season_m>a").get().forEach(function(a){
+                                animeEpisodeList.push(a.attribs["href"]);
+                            });
+                        }
+                        animeEpisodeList.forEach(function(animeEp){
+                            crawler.queue({
+                                uri: animeEp,
+                                retries: 0,
+                                maxConnections : 10,
+                                rateLimit: 1000,
+                                retryTimeout: 5000,
+                                callback: function (error, res, done) {
+                                    if(error)
+                                        return;
+                                    else
+                                    {
+                                        var $ = res.$;
+                                        var tmpUrl = $("iframe.metaframe", res.body).attr("src");
+                                        var episodeUrlEncoded = tmpUrl.substring(tmpUrl.indexOf("?p=")+3);
+                                        var episodeUrl = episodeUrlEncoded;
+                                        try 
+                                        {
+                                            episodeUrl = Base64.decode(episodeUrlEncoded);
+                                        }
+                                        catch(error){}
+                                        if(!episodeUrl.includes("//"))
+                                            episodeUrl = urls["repoUrl"] + episodeUrl;    
+                                        episodeUrl = episodeUrl.replace(/'/, "\\'")
+                                                                .replace(/tps:\/\//g, '');
+                                        console.log(episodeUrl);
+                                        if(episodeUrl!= null && episodeUrl.endsWith(".mp4"))
+                                            dbconn.query("INSERT INTO animesurls (animeurl) VALUES (\'" + urls["repoUrl"] + episodeUrl + "\');");
+                                    }
+                                    done();
+                                }
+                            });
+                        });
+                        done();
+                    }
                 });
-                
+            });            
         }
-
-        animeList.forEach(function(element){ //just a debug feature
-            console.log(element);
-        });
-
         done();
     }
 }]);
+console.log("finished");
 
 /**
  * Routing (cause, why not ?)
