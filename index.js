ef5c523511af7905477954bac17303625ba58c44
Panda <pandav0x@hotmail.fr>
2019-04-02 23:55:00 +0200

    changed lib from crawler to js-crawler

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = require('express')();
 const fs = require('fs');
 var http = require('http').Server(app);
 var mysql = require('mysql');
-var Crawler = require("crawler");
+var Crawler = require("js-crawler");
 var Base64 = require('base-64');
 
 var animeList = [];
@@ -38,21 +38,12 @@ var crawler = new Crawler();
  * Crawling
  */
 console.log("Finding anime list");
-crawler.queue([{
-    uri: urls["animeList"],
-    skipDuplicates: true,
-    retries: 0,
-    maxConnections : 1,
-    skipEventRequest: false,
-    callback: function (error, res, done) {
-        console.log("start retrieving anime list");
-        if(error)
-            console.log(error);
-        else
-        {
-            var $ = res.$;
-            //TODO: instert raw name in db
-            animeList = $(".anm_det_pop.pop_info")
+
+function getAnimeList()
+{
+    //uri: urls["animeList"],
+    //TODO: instert raw name in db
+    animeList = $(".anm_det_pop.pop_info")
                             .text()
                             .toLowerCase()
                             .replace(/!|:|;|\-|–|'|&|>|<|…|’|,|\?|"|`|%22/g, '')
@@ -64,70 +55,43 @@ crawler.queue([{
                             .filter(function(entry) { 
                                 return entry.trim() != ''; 
                             });
-            animeList.forEach(function(element){
-                if(element.includes("☆","♥","∞"))
-                    return;
-                crawler.queue({
-                    uri: (urls["animeBasePage"] + element),
-                    skipDuplicates: true,
-                    retries: 0,
-                    maxConnections : 5,
-                    rateLimit: 10000,
-                    retryTimeout: 1000,
-                    callback: function (error, res, done) {
-                        if(error)
-                            return;
-                        else
-                        {
-                            var $ = res.$;
-                            if($("article.item>div.poster>div.season_m>a") == null)
-                                return;
-                            $("article.item>div.poster>div.season_m>a").get().forEach(function(a){
-                                animeEpisodeList.push(a.attribs["href"]);
-                            });
-                        }
-                        animeEpisodeList.forEach(function(animeEp){
-                            crawler.queue({
-                                uri: animeEp,
-                                skipDuplicates: true,
-                                retries: 0,
-                                maxConnections : 10,
-                                rateLimit: 10000,
-                                retryTimeout: 5000,
-                                callback: function (error, res, done) {
-                                    if(error)
-                                        return;
-                                    else
-                                    {
-                                        var $ = res.$;
-                                        var tmpUrl = $("iframe.metaframe", res.body).attr("src");
-                                        var episodeUrlEncoded = tmpUrl.substring(tmpUrl.indexOf("?p=")+3);
-                                        var episodeUrl = episodeUrlEncoded;
-                                        try 
-                                        {
-                                            episodeUrl = Base64.decode(episodeUrlEncoded);
-                                        }
-                                        catch(error){}
-                                        if(!episodeUrl.includes("//"))
-                                            episodeUrl = urls["repoUrl"] + episodeUrl;    
-                                        episodeUrl = episodeUrl.replace(/'/, "\\'")
-                                                                .replace(/tps:\/\//g, '');
-                                        console.log(episodeUrl);
-                                        if(episodeUrl!= null && episodeUrl.endsWith(".mp4"))
-                                            dbconn.query("INSERT INTO animesurls (animeurl) VALUES (\'" + urls["repoUrl"] + episodeUrl + "\');");
-                                    }
-                                    done();
-                                }
-                            });
-                        });
-                        done();
-                    }
-                });
-            });            
-        }
-        done();
+    return;
+}
+
+function getEpisodeViewUrl()
+{
+    //element.includes("☆","♥","∞")
+    //animeList.forEach(function(element){
+    var $ = res.$;
+    if($("article.item>div.poster>div.season_m>a") == null)
+        return;
+    $("article.item>div.poster>div.season_m>a").get().forEach(function(a){
+        animeEpisodeList.push(a.attribs["href"]);
+    });
+    return;
+}
+
+function getUrls()
+{
+    //forEach(function(animeEp){
+    var $ = res.$;
+    var tmpUrl = $("iframe.metaframe", res.body).attr("src");
+    var episodeUrlEncoded = tmpUrl.substring(tmpUrl.indexOf("?p=")+3);
+    var episodeUrl = episodeUrlEncoded;
+    try 
+    {
+        episodeUrl = Base64.decode(episodeUrlEncoded);
     }
-}]);
+    catch(error){}
+    if(!episodeUrl.includes("//"))
+        episodeUrl = urls["repoUrl"] + episodeUrl;    
+    episodeUrl = episodeUrl.replace(/'/, "\\'")
+                            .replace(/tps:\/\//g, '');
+    console.log(episodeUrl);
+    if(episodeUrl!= null && episodeUrl.endsWith(".mp4"))
+        dbconn.query("INSERT INTO animesurls (animeurl) VALUES (\'" + urls["repoUrl"] + episodeUrl + "\');");
+    return;
+}
 
 /**
  * Routing (cause, why not ?)
