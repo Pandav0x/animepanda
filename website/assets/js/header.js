2f616d66076202d174a06028a3739cf5001e4a14
Panda <pandav0x@hotmail.fr>
2020-06-13 20:31:53 +0200

    Add strict equality

--- website/assets/js/header.js
+++ website/assets/js/header.js
@@ -3,7 +3,7 @@
 let explodedUrl = (window.location + '').split('/');
 let currentPage = explodedUrl[explodedUrl.length - 2] + "s";
 
-if(explodedUrl.length == 4)
+if(explodedUrl.length === 4)
     currentPage= "homepage";
 
 if(document.querySelector("[id$='" + currentPage  + "']") !== null){
@@ -12,6 +12,6 @@ if(document.querySelector("[id$='" + currentPage  + "']") !== null){
 
 document.getElementById("nav-input-button").addEventListener("click", function(){
     let search = document.getElementById("nav-input-search").value.trim();
-    if(search != "")
+    if(search !== '')
         console.log(search);
 });
